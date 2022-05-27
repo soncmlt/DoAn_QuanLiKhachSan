1b2fd64f28700a8ea77ac55a00920533dfcf8286
Huynh Khang <huynhvirgo@gmail.com>
2022-05-27 15:53:39 +0700

    Commit Code

--- DAO_BLL/Properties/Settings.Designer.cs
+++ DAO_BLL/Properties/Settings.Designer.cs
@@ -8,29 +8,35 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DAO_BLL.Properties {
-    
-    
+namespace DAO_BLL.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
             }
         }
-        
+
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         //[global::System.Configuration.DefaultSettingValueAttribute("Data Source=162860-VHBSON;Initial Catalog=QL_KhachSan;Integrated Security=True")]
-        //[global::System.Configuration.DefaultSettingValueAttribute("Data Source=MSI\\HQKHANG;Initial Catalog=QL_KhachSan;Integrated Security=True")]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-4NBM1DU;Initial Catalog=QL_KhachSan;User ID=sa;Password=sa2019")]
-        public string QL_KhachSanConnectionString {
-            get {
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MSI\\HQKHANG;Initial Catalog=QL_KhachSan;Integrated Security=True")]
+        //[global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-4NBM1DU;Initial Catalog=QL_KhachSan;User ID=sa;Password=sa2019")]
+        public string QL_KhachSanConnectionString
+        {
+            get
+            {
                 return ((string)(this["QL_KhachSanConnectionString"]));
             }
         }
